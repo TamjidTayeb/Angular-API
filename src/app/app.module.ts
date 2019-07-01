5a83e7c1b38badc88c097b35ca7953acc8e2c829
tamjidtayeb <tayebtamjid@gmail.com>
2019-07-01 10:59:03 +1000

    added dynamic classes for completed tasks

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { TodosComponent } from './components/todos/todos.component';
-import { TodoItemComponent } from './component/todo-item/todo-item.component';
+import { TodoItemComponent } from './components/todo-item/todo-item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TodosComponent,
-    TodoItemComponent
+    TodoItemComponent,
   ],
   imports: [
     BrowserModule
